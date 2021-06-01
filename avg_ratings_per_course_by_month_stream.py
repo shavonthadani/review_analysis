7fc2b737f067e9bac7f8fe3e08cc92af6612fe1e
shavonthadani <shavon.thadani@gmail.com>
2021-06-01 11:48:41 -0400

    Create avg_ratings_per_course_by_month_stream.py
    Created a stream graph that shows the average rating per course by month.

---
+++ avg_ratings_per_course_by_month_stream.py
@@ -0,0 +1,336 @@
+import justpy as jp
+import pandas
+from datetime import datetime
+from pytz import utc
+
+#creates dataframe
+data = pandas.read_csv("reviews.csv",parse_dates=["Timestamp"])
+#adds a column for the week the review was made through using the timestamp column
+data["Month"] = data["Timestamp"].dt.strftime("%Y-%m")
+#Collects the data into groups by course and month and has a column for rating mean
+month_average = data.groupby(["Month", "Course Name"])["Rating"].mean().unstack()
+#js code from https://www.highcharts.com/docs/chart-and-series-types/spline-chart copied the json portion from HighCharts
+#js code for spline chart
+#triple quote string allows the string to be multi lined and not get confused with double/single quotes within it
+chart_def = """
+ {
+
+    chart: {
+        type: 'streamgraph',
+        marginBottom: 30,
+        zoomType: 'x'
+    },
+
+    title: {
+        floating: true,
+        align: 'left',
+        text: 'Winter Olympic Medal Wins'
+    },
+    subtitle: {
+        floating: true,
+        align: 'left',
+        y: 30,
+    },
+
+    xAxis: {
+        maxPadding: 0,
+        type: 'category',
+        crosshair: true,
+        categories: [],
+        labels: {
+            align: 'left',
+            reserveSpace: false,
+            rotation: 270
+        },
+        lineWidth: 0,
+        margin: 20,
+        tickWidth: 0
+    },
+
+    yAxis: {
+        visible: false,
+        startOnTick: false,
+        endOnTick: false
+    },
+
+    legend: {
+        enabled: false
+    },
+
+    annotations: [{
+        labelOptions: {
+            backgroundColor: 'rgba(255,255,255,0.5)',
+            borderColor: 'silver'
+        }
+    }],
+
+    plotOptions: {
+        series: {
+            label: {
+                minFontSize: 5,
+                maxFontSize: 15,
+                style: {
+                    color: 'rgba(255,255,255,0.75)'
+                }
+            }
+        }
+    },
+
+    // Data parsed with olympic-medals.node.js
+    series: [{
+        name: "Finland",
+        data: [
+            0, 11, 4, 3, 6, 0, 0, 6, 9, 7, 8, 10, 5, 5, 7, 9, 13, 7, 7, 6, 12, 7, 9, 5, 5
+        ]
+    }, {
+        name: "Austria",
+        data: [
+            0, 3, 4, 2, 4, 0, 0, 8, 8, 11, 6, 12, 11, 5, 6, 7, 1, 10, 21, 9, 17, 17, 23, 16, 17
+        ]
+    }, {
+        name: "Sweden",
+        data: [
+            0, 2, 5, 3, 7, 0, 0, 10, 4, 10, 7, 7, 8, 4, 2, 4, 8, 6, 4, 3, 3, 7, 14, 11, 15
+        ]
+    }, {
+        name: "Norway",
+        data: [
+            0, 17, 15, 10, 15, 0, 0, 10, 16, 4, 6, 15, 14, 12, 7, 10, 9, 5, 20, 26, 25, 25, 19, 23, 26
+        ]
+    }, {
+        name: "U.S.",
+        data: [
+            0, 4, 6, 12, 4, 0, 0, 9, 11, 7, 10, 7, 7, 8, 10, 12, 8, 6, 11, 13, 13, 34, 25, 37, 28
+        ]
+    }, {
+        name: "East Germany",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 14, 19, 23, 24, 25, 0, 0, 0, 0, 0, 0, 0
+        ]
+    }, {
+        name: "West Germany",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 5, 10, 5, 4, 8, 0, 0, 0, 0, 0, 0, 0
+        ]
+    }, {
+        name: "Germany",
+        data: [
+            0, 0, 1, 2, 6, 0, 0, 0, 7, 2, 8, 9, 0, 0, 0, 0, 0, 0, 26, 24, 29, 36, 29, 30, 19
+        ]
+    }, {
+        name: "Netherlands",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 2, 2, 9, 9, 6, 4, 0, 7, 4, 4, 11, 8, 9, 8, 24
+        ]
+    }, {
+        name: "Italy",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 1, 4, 4, 5, 4, 2, 2, 5, 14, 20, 10, 13, 11, 5, 8
+        ]
+    }, {
+        name: "Canada",
+        data: [
+            0, 1, 1, 7, 1, 0, 0, 3, 2, 3, 4, 3, 3, 1, 3, 2, 4, 5, 7, 13, 15, 17, 24, 26, 25
+        ]
+    }, {
+        name: "Switzerland",
+        data: [
+            0, 3, 1, 1, 3, 0, 0, 10, 2, 6, 2, 0, 6, 10, 5, 5, 5, 15, 3, 9, 7, 11, 14, 9, 11
+        ]
+    }, {
+        name: "Great Britain",
+        data: [
+            0, 4, 1, 0, 3, 0, 0, 2, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 2, 1, 2, 1, 1, 4
+        ]
+    }, {
+        name: "France",
+        data: [
+            0, 3, 1, 1, 1, 0, 0, 5, 1, 0, 3, 7, 9, 3, 1, 1, 3, 2, 9, 5, 8, 11, 9, 11, 15
+        ]
+    }, {
+        name: "Hungary",
+        data: [
+            0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        ]
+    }, {
+        name: "Unified Team",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 23, 0, 0, 0, 0, 0, 0
+        ]
+    }, {
+        name: "Soviet Union",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 16, 21, 25, 13, 16, 27, 22, 25, 29, 0, 0, 0, 0, 0, 0, 0
+        ]
+    }, {
+        name: "Russia",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 23, 18, 13, 22, 15, 33
+        ]
+    }, {
+        name: "Japan",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 3, 0, 1, 1, 1, 7, 5, 10, 2, 1, 5, 8
+        ]
+    }, {
+        name: "Czechoslovakia",
+        data: [
+            0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 4, 3, 1, 1, 6, 3, 3, 0, 0, 0, 0, 0, 0
+        ]
+    }, {
+        name: "Poland",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 2, 2, 6, 6
+        ]
+    }, {
+        name: "Spain",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0
+        ]
+    }, {
+        name: "China",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 8, 8, 11, 11, 9
+        ]
+    }, {
+        name: "South Korea",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 6, 6, 4, 11, 14, 8
+        ]
+    }, {
+        name: "Czech Republic",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 4, 6, 8
+        ]
+    }, {
+        name: "Belarus",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 1, 1, 3, 6
+        ]
+    }, {
+        name: "Kazakhstan",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 2, 0, 0, 1, 1
+        ]
+    }, {
+        name: "Bulgaria",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 3, 1, 0, 0
+        ]
+    }, {
+        name: "Denmark",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0
+        ]
+    }, {
+        name: "Ukraine",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 0, 2, 0, 2
+        ]
+    }, {
+        name: "Australia",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 2, 3, 3
+        ]
+    }, {
+        name: "Belgium",
+        data: [
+            0, 1, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0
+        ]
+    }, {
+        name: "Romania",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        ]
+    }, {
+        name: "Liechtenstein",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 4, 2, 1, 0, 0, 0, 0, 0, 0, 0
+        ]
+    }, {
+        name: "Yugoslavia",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 3, 0, 0, 0, 0, 0, 0, 0
+        ]
+    }, {
+        name: "Luxembourg",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0
+        ]
+    }, {
+        name: "New Zealand",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0
+        ]
+    }, {
+        name: "North Korea",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0
+        ]
+    }, {
+        name: "Slovakia",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 3, 1
+        ]
+    }, {
+        name: "Croatia",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 3, 3, 1
+        ]
+    }, {
+        name: "Slovenia",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 1, 0, 3, 8
+        ]
+    }, {
+        name: "Latvia",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 4
+        ]
+    }, {
+        name: "Estonia",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 1, 0
+        ]
+    }, {
+        name: "Uzbekistan",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0
+        ]
+    }],
+
+    exporting: {
+        sourceWidth: 800,
+        sourceHeight: 600
+    }
+
+}
+"""
+def app():
+    #create web page
+    wp = jp.QuasarPage()
+    #order of elements determines where it will be in web page
+    #the classes variable allows to change styling. See https://quasar.dev/style for how to
+    #heading
+    h1 = jp.QDiv(a=wp, text="Analysis of Course Reviews", classes="text-h3 text-center q-pa-md")
+    #paragraph
+    p1 = jp.QDiv(a=wp, text="These graphs represent course review analysis")
+    #JP.HighCharts converts chart_def to dictionary so python can understand and puts it
+    #the dictionary is makes is different then a normal one. Can access keys inside using dot notation
+    hc = jp.HighCharts(a=wp, options=chart_def)
+
+    #options becomes an attribute of hc as the dictionary.
+    #then, accessing title key then text and changing the element to average rating by Day
+    hc.options.title.text = "Average Rating per Course by Month"
+    #Changing Data
+    #series is a list which has a dictionary in it with name and data
+    #giving x axis data
+    #dates are not considered numbers in high charts but rather as categories
+    hc.options.xAxis.categories = list(month_average.index)
+    #Adding the data
+    #for loop adding dics to the list with v1 names
+    #for loop inside a list in a dic to make a list of data
+    hc_data = [{"name":v1, "data":[v2 for v2 in month_average[v1]]} for v1 in month_average.columns]
+    hc.options.series = hc_data
+    return wp
+jp.justpy(app)
